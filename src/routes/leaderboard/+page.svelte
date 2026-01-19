e67bea20c332331ef0aa779952842e9800ef916a
Echo <echopossum@gmail.com>
2026-01-18 20:20:34 -0500

    Complete leaderboard layout

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -2,8 +2,25 @@
 	import { getLeaderboard } from './leaderboard.remote';
 </script>
 
-{#each await getLeaderboard() as slot (slot.id)}
-	<div>
-		{slot.firstName}, {slot.lastName}, {slot.total}
+<svelte:boundary>
+	<div class="flex h-screen w-screen flex-col items-center justify-start bg-black">
+		{#each await getLeaderboard() as slot (slot.id)}
+			<div class="flex w-screen items-center justify-between odd:bg-base-300 even:bg-base-100">
+				<div class="flex w-sm justify-start p-2 pl-8 text-2xl">
+					Name:
+					{slot.firstName}
+					{slot.lastName}
+				</div>
+				<div class="flex w-sm justify-center p-2 text-2xl">
+					Unit:
+					{slot.unitType}
+					{slot.unitNumber}
+				</div>
+				<div class="flex w-sm justify-end p-2 pr-8 text-2xl">
+					Score:
+					{slot.total}
+				</div>
+			</div>
+		{/each}
 	</div>
-{/each}
+</svelte:boundary>
