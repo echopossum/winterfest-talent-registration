48a73f8ce699295f8dadb5aa834fe162d1f19a10
Echo <echopossum@gmail.com>
2026-01-30 16:30:38 -0600

    Add header bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,7 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<div class="mb-4 flex h-14 w-full items-center justify-center bg-base-300 sm:justify-start">
+	<h1 class="p-4 text-2xl">Winterfest Talent Show</h1>
+</div>
 {@render children()}
