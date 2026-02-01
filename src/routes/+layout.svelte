c79c906635b5786a2ebb747c525d125635ffe4ca
Echo <echopossum@gmail.com>
2026-02-01 14:56:01 -0500

    Add 50th logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.png';
-
+	import logo from '$lib/assets/wf-50-white.png';
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<div class="mb-4 flex h-14 w-full items-center justify-center bg-base-300 sm:justify-start">
+<div class="flex h-14 w-full items-center justify-center bg-base-300 sm:justify-between">
 	<h1 class="p-4 text-2xl">Winterfest Talent Show</h1>
+	<img src={logo} alt="" class=" h-10 object-contain pr-4" />
 </div>
 {@render children()}
