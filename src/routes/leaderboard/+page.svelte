c103275c2ac15fa9dc26547803f6f6f520d9a2cf
Echo <echopossum@gmail.com>
2026-02-04 22:18:13 -0500

    Make it so registrants with no scores are not rendered on leaderboard

--- src/routes/leaderboard/+page.svelte
+++ src/routes/leaderboard/+page.svelte
@@ -9,33 +9,29 @@
 			invalidateAll();
 		});
 	});
-
-	// onMount(async () => {
-	// 	setInterval(() => {
-	// 		invalidateAll();
-	// 	}, 10000);
-	// });
 </script>
 
 <svelte:boundary>
 	<div class="flex min-h-dvh w-full flex-col items-center justify-start bg-black">
 		{#each await getLeaderboard() as slot (slot.id)}
-			<div class="flex w-screen items-center justify-between odd:bg-base-100 even:bg-base-300">
-				<div class="flex w-sm justify-start p-2 pl-8 text-2xl">
-					Name:
-					{slot.firstName}
-					{slot.lastName}
+			{#if slot.total > 0}
+				<div class="flex w-screen items-center justify-between odd:bg-base-100 even:bg-base-300">
+					<div class="flex w-sm justify-start p-2 pl-8 text-2xl">
+						Name:
+						{slot.firstName}
+						{slot.lastName}
+					</div>
+					<div class="flex w-sm justify-center p-2 text-2xl">
+						Unit:
+						{slot.unitType}
+						{slot.unitNumber}
+					</div>
+					<div class="flex w-sm justify-end p-2 pr-8 text-2xl">
+						Score:
+						{slot.total}
+					</div>
 				</div>
-				<div class="flex w-sm justify-center p-2 text-2xl">
-					Unit:
-					{slot.unitType}
-					{slot.unitNumber}
-				</div>
-				<div class="flex w-sm justify-end p-2 pr-8 text-2xl">
-					Score:
-					{slot.total}
-				</div>
-			</div>
+			{/if}
 		{/each}
 	</div>
 </svelte:boundary>
